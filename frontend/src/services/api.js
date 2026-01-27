8e07e35c728ef6595d76d3d4c9036cfe41f2881d
back-up-joe <jmcontrerass@gmail.com>
2026-01-27 13:26:35 -0300

    config api env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 
 // Configura la URL base de API Django
-// const API_BASE_URL = 'http://localhost:8000/api';
+// const API_BASE_URL = 'http://localhost:8000/api/';
 
-const API_BASE_URL = REACT_APP_API_URL;
-
-// const API_BASE_URL = process.env.REACT_APP_API_URL;
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 console.log('API_BASE_URL desde .env:', API_BASE_URL);  // Añade esta línea
 
 const api = axios.create({
+// const apiBackend = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
   headers: {
@@ -22,7 +21,8 @@ export const articleService = {
   getArticles: async () => {
     try {
       console.log('Haciendo petición a:', `${API_BASE_URL}/articles/`);
-      const response = await api.get('/articles/');
+      const response = await api.get('articles/');
+      // const response = await apiBackend.get('articles/');
       console.log('API Response STATUS:', response.status);
       console.log('API Response HEADERS:', response.headers);
       
