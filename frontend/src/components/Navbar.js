bbc10c3ac24caffdadfa638fcde7a19ca7bc6a39
back-up-joe <jmcontrerass@gmail.com>
2026-04-03 11:29:21 -0300

    título en mayúscula

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg navbar-dark bg-danger shadow">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            Resistencia Informativa
+            RESISTENCIA INFORMATIVA
           </Link>
         </div>
       </nav>
