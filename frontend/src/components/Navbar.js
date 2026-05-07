ceb5fbd97597ff5b94d595f61083c213a1072230
back-up-joe <jmcontrerass@gmail.com>
2026-05-06 20:31:13 -0400

    ícono instagram, imagen de instagram, backup sql

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,6 +10,17 @@ const Navbar = () => {
           <Link className="navbar-brand" to="/">
             RESISTENCIA INFORMATIVA
           </Link>
+
+          {/* Solo ícono de Instagram */}
+          <a 
+            href="https://www.instagram.com/resistencia_informativa_org/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="text-white ms-auto"
+            style={{ fontSize: '1.5rem' }}
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
         </div>
       </nav>
       <SearchBar />
